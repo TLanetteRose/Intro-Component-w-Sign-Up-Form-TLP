a5ab840ee765e1b9a7b052ed11d68fb7a6458142
T. Lanette <TLPollard4516@gmail.com>
2020-07-28 15:58:20 -0400

    Added JS validity for form
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/script.js
+++ js/script.js
@@ -4,7 +4,44 @@ const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
+// Add eventlistener for submitting form
+form.addEventListener('submit', e => {
+    //Prevents submittion of form by cancelling
+    e.preventDefault();
+    checkInput();
+})
 
+// Function to check inputs in forms 
+function checkInput() {
+    //Trim whitespace 
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    // Check conditions for input
+    if (firstNameValue === '') {
+        setErrorMsg(firstName);
+    } else {
+        setSuccess(firstName);
+    }
+    if (lastNameValue === '') {
+        setErrorMsg(lastName);
+    } else {
+        setSuccess(lastName);
+    }
+    if (emailValue === '') {
+        setErrorMsg(email);
+    } else {
+        setSuccess(email);
+    }
+    if (passwordValue === '') {
+        setErrorMsg(password);
+    } else {
+        setSuccess(password);
+    }
+
+}
 
 
 
